d1024dfe3b80ccb97a7b6d2a8ce1091870086d18
thelittlemermaid <arielinman98@gmail.com>
2019-10-20 19:18:39 -0500

    detailed content now included

--- android/components/MissingCard/index.js
+++ android/components/MissingCard/index.js
@@ -7,7 +7,6 @@ import MissingList from '../MissingList';
 const MissingCard = ({firstName, middleName, lastName, thumbnailUrl, missingCounty, missingCity, missingCountry, missingDate, caseNumber, orgPrefix, navigation}) =>  {
     let shortenedUrl = thumbnailUrl.substring(0, thumbnailUrl.length - 5);
     let newUrl = 'https://api.missingkids.org' + shortenedUrl + '.jpg';
-    console.log(newUrl);
 
     return (
         <TouchableOpacity style={{backgroundColor: 'transparent'}} onPress={() => navigation.navigate('MissingPerson', {firstName, middleName, lastName, missingCounty, missingCity, missingCountry, missingDate, caseNumber, orgPrefix, newUrl})}>
--- android/components/MissingPerson/index.js
+++ android/components/MissingPerson/index.js
@@ -1,12 +1,16 @@
 import React, { PureComponent } from 'react'; 
 import { View, Text, Image } from 'react-native';
 import styles from './styles';
+import { ScrollView } from 'react-native-gesture-handler';
 
 
 export default class MissingPerson extends PureComponent {
 
     state = {
-        detailedData: []
+        detailedData: [],
+        heightMetric: "",
+        weightMetric: "",
+        determinedAge: 0
     }
 
     async componentDidMount() {
@@ -16,31 +20,58 @@ export default class MissingPerson extends PureComponent {
             .then(function(response) {return response.json();})
             .then(function(myJson) {
                 this.setState({detailedData: myJson.childBean});
-            }.bind(this))
-            console.log(detailedData);
+                this.determineMetrics();
+            }.bind(this));
         } catch(err) {
             console.log("Error fetching data-----------", err);
         }
         
     }
 
+    determineMetrics() {
+        const { detailedData } = this.state;
+
+        if(detailedData.heightInInch) {
+            this.setState({heightMetric: " inches"});
+        } else {
+            this.setState({heightMetric: " mm"});
+        }
+
+        if(detailedData.weightInPound) {
+            this.setState({weightMetric: " pounds"});
+        } else {        
+            this.setState({weightMetric: " kilograms"});
+        }
+
+        if(detailedData.age == -1) {
+            this.setState({determinedAge: detailedData.approxAge});
+        } else {
+            this.setState({determinedAge: detailedData.age});
+        }
+    }
+
     render() {
         const { navigation } = this.props;
-        /* fetch("https://api.missingkids.org/missingkids/servlet/JSONDataServlet?action=childDetail&caseNum=" + navigation.getParam('caseNumber') + "&orgPrefix=" + navigation.getParam('orgPrefix'))
-            .then(function(response) {return response.json();})
-            .then(function(myJson) {
-                this.setState({detailedData: myJson.childBean});
-            }.bind(this))
-
-            console.log({detailedData}); */
+        const { detailedData, heightMetric, weightMetric, determinedAge } = this.state;
 
         return (
-            <View>
+            <ScrollView>
                 <Text style={styles.personName}>{navigation.getParam('firstName')} {navigation.getParam('middleName')} {navigation.getParam('lastName')}</Text>
-                <Text>{navigation.getParam('caseNumber')}</Text>
-                <Text>Place Where {navigation.getParam('firstName')} Went Missing: {navigation.getParam('missingCounty')} {navigation.getParam('missingCity')} {navigation.getParam('missingCountry')}</Text>
+                <Text>Case Number: {navigation.getParam('caseNumber')}</Text>
+                <Text>Place Where {navigation.getParam('firstName')} Went Missing: {navigation.getParam('missingCounty')} {navigation.getParam('missingCity')} {detailedData.missingState} {navigation.getParam('missingCountry')}</Text>
                 <Text>{navigation.getParam('firstName')} Went Missing On: {navigation.getParam('missingDate')}</Text>
-            </View>
+                <Text>Approximate Age: {determinedAge}</Text>
+                <Text>Height: {detailedData.height}{heightMetric}</Text>
+                <Text>Weight: {detailedData.weight}{weightMetric}</Text>
+                <Text>Eye Color: {detailedData.eyeColor}</Text>
+                <Text>Hair Color: {detailedData.hairColor}</Text>
+                <Text>Race: {detailedData.race}</Text>
+                <Text>Sex: {detailedData.sex}</Text>
+                <Text>If you have a tip, please contact: {detailedData.altContact} {"\n"}</Text>
+
+                <Text>Case Type: {detailedData.caseType}</Text>
+                <Text>Case Circumstance: {detailedData.circumstance}</Text>
+            </ScrollView>
         );
     }
 }
