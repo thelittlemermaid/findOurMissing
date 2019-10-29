5248019f098348dc0dfe29cd10465a1f64f05b40
thelittlemermaid <42946818+thelittlemermaid@users.noreply.github.com>
2019-10-28 20:26:46 -0500

    merge locationTesting branch
    * experiment with creating detailed page
    
    * resolve nav library
    
    * add react nav stack
    
    * tiles now have navigation
    
    * detailed content now included
    
    * images in detail
    
    * altered "no" photos and added link
    
    * altering page reading
    
    * Revert "altering page reading"
    
    This reverts commit 8d5077797d88646a9f730d71d05397121c811b10.
    
    * Geolocation now gives coordinates

--- android/components/MissingList/index.js
+++ android/components/MissingList/index.js
@@ -1,5 +1,6 @@
-import React, { PureComponent } from 'react';
-import { View, Image, FlatList, TouchableOpacity, Text, ActivityIndicator } from 'react-native';
+import React, { PureComponent, Fragment } from 'react';
+import { FlatList, Button, Text, ActivityIndicator, Linking } from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
 import styles from './styles';
 import MissingCard from '../MissingCard';
 
@@ -7,6 +8,8 @@ export default class MissingList extends PureComponent {
     state = {
         missingList: [],
         loading: true,
+        latitude: 0,
+        longitude: 0
     }
 
     static navigationOptions = {
@@ -15,6 +18,11 @@ export default class MissingList extends PureComponent {
     
     async componentDidMount() {
         try {
+            Geolocation.getCurrentPosition(info => {
+                this.setState({latitude: info.coords.latitude, longitude: info.coords.longitude});
+                console.log(this.state.latitude);
+                console.log(this.state.longitude);
+            });
             city = "Hollywood"
             state = "CA"
             country = "US"
@@ -32,11 +40,17 @@ export default class MissingList extends PureComponent {
         const { missingList, loading } = this.state;
         const { navigation } = this.props;
         if(!loading) {
-            return <FlatList 
+            return (
+                <Fragment>
+                    <FlatList 
                     data={missingList}
                     renderItem={(data) => <MissingCard {...data.item} navigation={navigation} />}
                     keyExtractor={(item, index) => index.toString()} 
                     />
+                    <Text style={styles.linkText}>To find more missing person cases from all over the world tap here:</Text>
+                    <Button title="National Center for Missing & Exploited Children" onPress={ ()=>{ Linking.openURL('http://api.missingkids.org/home')}}></Button>
+                </Fragment>
+                );
         } else {
             return <ActivityIndicator />
         }
